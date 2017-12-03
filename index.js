075a6e58b5fa38883318e7b9a9d9cf907928a62a
Måns Dahlström <Mns Dahlstrm>
2017-12-03 22:01:53 +0100

    printComments function added, getDifferancePerQuestioAdded

--- index.js
+++ index.js
@@ -34,6 +34,132 @@ function getAvg(data){
   print(data, output);
 }
 
+function getDifferancePerQuestion(data) {
+  const output = [];
+  output.q1 = { high: 0, low: 10 };
+  output.q2 = { high: 0, low: 10 };
+  output.q3 = { high: 0, low: 10 };
+  output.q4 = { high: 0, low: 10 };
+  output.q5 = { high: 0, low: 10 };
+  output.q6 = { high: 0, low: 10 };
+  output.q7 = { high: 0, low: 10 };
+  output.q8 = { high: 0, low: 10 };
+  
+  const HighOutput = {
+    q1: 0,
+    q2: 0,
+    q3: 0,
+    q4: 0,
+    q5: 0,
+    q6: 0,
+    q7: 0,
+    q8: 0, 
+  }
+  const LowOutput = {
+    q1: 0,
+    q2: 0,
+    q3: 0,
+    q4: 0,
+    q5: 0,
+    q6: 0,
+    q7: 0,
+    q8: 0, 
+  }
+
+  data.map(item => {
+    output['q1'] = {
+      high: Math.max(output['q1'].high, item.res1),
+      low: Math.min(output['q1'].low, item.res1),
+    }
+    output['q2'] = {
+      high: Math.max(output['q2'].high, item.Res2),
+      low: Math.min(output['q2'].low, item.Res2),
+    }
+    output['q3'] = {
+      high: Math.max(output['q3'].high, item.Res3),
+      low: Math.min(output['q3'].low, item.Res3),
+    }
+    output['q4'] = {
+      high: Math.max(output['q4'].high, item.Res4),
+      low: Math.min(output['q4'].low, item.Res4),
+    }
+    output['q5'] = {
+      high: Math.max(output['q5'].high, item.Res5),
+      low: Math.min(output['q5'].low, item.Res5),
+    }
+    output['q6'] = {
+      high: Math.max(output['q6'].high, item.Res6),
+      low: Math.min(output['q6'].low, item.Res6),
+    }
+    output['q7'] = {
+      high: Math.max(output['q7'].high, item.Res7),
+      low: Math.min(output['q7'].low, item.Res7),
+    }
+    output['q8'] = {
+      high: Math.max(output['q8'].high, item.Res8),
+      low: Math.min(output['q8'].low, item.Res8),
+    }
+  });
+
+  data.map(item => {
+    if (item.res1 === output['q1'].high) {
+      HighOutput.q1 += 1; 
+    }
+    if (item.res1 === output['q1'].low) {
+      LowOutput.q1 += 1;
+    }
+    if (item.Res2 === output['q2'].high) {
+      HighOutput.q2 += 1; 
+    }
+    if (item.Res2 === output['q2'].low) {
+      LowOutput.q2 += 1;
+    }
+    if (item.Res3 === output['q3'].high) {
+      HighOutput.q3 += 1; 
+    }
+    if (item.Res3 === output['q3'].low) {
+      LowOutput.q3 += 1;
+    }
+    if (item.Res4 === output['q4'].high) {
+      HighOutput.q4 += 1; 
+    }
+    if (item.Res4 === output['q4'].low) {
+      LowOutput.q4 += 1;
+    }
+    if (item.Res5 === output['q5'].high) {
+      HighOutput.q5 += 1; 
+    }
+    if (item.Res5 === output['q5'].low) {
+      LowOutput.q5 += 1;
+    }
+    if (item.Res6 === output['q6'].high) {
+      HighOutput.q6 += 1; 
+    }
+    if (item.Res6 === output['q6'].low) {
+      LowOutput.q6 += 1;
+    }
+    if (item.Res7 === output['q7'].high) {
+      HighOutput.q7 += 1; 
+    }
+    if (item.Res7 === output['q7'].low) {
+      LowOutput.q7 += 1;
+    }
+    if (item.Res8 === output['q8'].high) {
+      HighOutput.q8 += 1; 
+    }
+    if (item.Res8 === output['q8'].low) {
+      LowOutput.q8 += 1;
+    }
+
+  })
+  console.log(LowOutput)
+  console.log(HighOutput);
+  Object.keys(output).map(key => {
+    console.log('---', key, '---')
+    console.log(output[key]);
+  })
+  return output;
+}
 
 function groupByUsage(data){
   const output = [];
@@ -80,25 +206,21 @@ function groupByUsage(data){
   return output;
 }
 
+function printComments(data){
+  // Abc is placeholder data; since ther parser will not accept 
+  // empty rows as a valid object. 
+  const comments = data.filter(item => item.Res9 !== 'abc');
+  console.log(comments.map(item => console.log(item.Res9)))
+}
+
 const age1 = data.filter(item => Number(item.Age) <= 29);
 const age2 = data.filter(item => Number(item.Age) < 40 && Number(item.Age) >= 30);
 const age3 = data.filter(item => Number(item.Age) >= 40);
 const test4 = data.filter(item => Number(item.Res8) === 1);
 
+
 console.log('---- ALL ----');
 getAvg(data);
+// groupByUsage(data);
 
-// console.log('---- Age < 30 ----');
-// getAvg(age1);
-
-// console.log('---- Age 30-40 ----');
-// getAvg(age2);
-
-// console.log('---- Age > 40 ----');
-// getAvg(age3);
-
-// console.log('---- where awnser 1 = 1 ----');
-// getAvg(test4);
-
-
-groupByUsage(data);
+getDifferancePerQuestion(data);
